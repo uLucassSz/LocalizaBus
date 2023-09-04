8b9f35f7b8b40d00c93d610c418bb117f53d5cf3
lucaix <lucasfainerxd@gmail.com>
2023-09-03 23:59:01 -0300

    js login

---
+++ html/pages/Login/script.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    document.querySelector('.login-box.register').style.display = 'none';
+    document.querySelector('.login-box.login').style.display = 'block';
+}
+
+function showRegister() {
+    document.querySelector('.login-box.login').style.display = 'none';
+    document.querySelector('.login-box.register').style.display = 'block';
+}
