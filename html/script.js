21c87e8008b4deaded80d29910eff58186e0a260
lucaix <lucasfainerxd@gmail.com>
2023-09-04 10:59:47 -0300

    navbar

---
+++ html/script.js
@@ -0,0 +1,9 @@
+navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `
+    <span class="logo"><i class="bx bxs-bus"> </i>Localiza Bus</span>
+
+    <div class="btn-actions">
+        <img src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg" alt="">
+        <span class="name">Lucaix</span>
+    </div>`
