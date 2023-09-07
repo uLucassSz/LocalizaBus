ba9a1c3052d9905a172d12ffddaa1b7baf629609
lucaix <lucasfainerxd@gmail.com>
2023-09-07 00:55:33 -0300

    updates

--- html/scripts/routes.js
+++ html/scripts/routes.js
@@ -5,16 +5,16 @@ const linha = linhas.find(d => d.id == params.get("linha"))
 if (!linha)
     location.href = location.origin
 
-const titulo_linha = document.getElementById("titulo_rua")
-titulo_linha.innerText = `${linha.name} | ${linha.street}`
+const titleStreet = document.getElementById("titulo_rua")
+titleStreet.innerText = `${linha.name} | ${linha.street}`
 
 select.innerHTML = linha.locales.map(l => `<option value="${l.name}">${l.name}</option>`).join("")
 changeRoutes(select)
 
 function changeRoutes(doc) {
     const div = document.getElementById("linhas")
-    const titulo_linha = document.getElementById("titulo_linha")
-    titulo_linha.innerText = doc.value
+    const titleLine = document.getElementById("titulo_linha")
+    titleLine.innerText = doc.value
 
     document.getElementById("select").innerHTML = linha.locales.map(l => `<option value="${l.name}" ${l.name == doc.value ? "selected" : ""}>${l.name}</option>`).join("")
 
